7beb4ed85e556cf09351419d009d7ba2b375db79
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-04-08 20:40:36 +0200

    Add settings screen fragment and layout with navigation to reminder settings

---
+++ app/src/main/java/com/petplanner/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.petplanner
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val openReminderButton = view.findViewById<Button>(R.id.openReminderSettingsButton)
+        openReminderButton.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.navHostFragment, ReminderSettingsFragment())
+                .addToBackStack(null)
+                .commit()
+        }
+    }
+}
