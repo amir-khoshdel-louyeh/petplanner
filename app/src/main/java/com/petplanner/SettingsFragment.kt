20e7880a2a249da98aadc0ca56fefb6fd12fa9f4
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-04-09 15:43:11 +0200

    Polish onboarding UI and display saved language in settings

--- app/src/main/java/com/petplanner/SettingsFragment.kt
+++ app/src/main/java/com/petplanner/SettingsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class SettingsFragment : Fragment() {
@@ -18,7 +19,11 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val languageValue = view.findViewById<TextView>(R.id.languageValue)
         val openReminderButton = view.findViewById<Button>(R.id.openReminderSettingsButton)
+
+        languageValue.text = OnboardingPreferences.getLanguage(requireContext())
+
         openReminderButton.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.navHostFragment, ReminderSettingsFragment())
