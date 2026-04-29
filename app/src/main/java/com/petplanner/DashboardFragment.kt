77a72f89517cd6fe2342f50f2d1b5d9487d728f8
amir-khoshdel-louyeh <amirkhoshdellouyeh@gmail.com>
2026-04-29 20:12:55 +0200

    Add To-Do List section with upcoming/completed tabs to dashboard

--- app/src/main/java/com/petplanner/DashboardFragment.kt
+++ app/src/main/java/com/petplanner/DashboardFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 
 class DashboardFragment : Fragment() {
     private lateinit var greetingText: TextView
+    private lateinit var todoTabLayout: TabLayout
+    private lateinit var upcomingContent: LinearLayout
+    private lateinit var completedContent: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +28,7 @@ class DashboardFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bindViews(view)
+        setupTodoTabs()
         loadDashboard()
     }
 
@@ -34,6 +39,31 @@ class DashboardFragment : Fragment() {
 
     private fun bindViews(view: View) {
         greetingText = view.findViewById(R.id.greetingText)
+        todoTabLayout = view.findViewById(R.id.todoTabLayout)
+        upcomingContent = view.findViewById(R.id.upcomingContent)
+        completedContent = view.findViewById(R.id.completedContent)
+    }
+
+    private fun setupTodoTabs() {
+        todoTabLayout.addTab(todoTabLayout.newTab().setText(R.string.todo_upcoming))
+        todoTabLayout.addTab(todoTabLayout.newTab().setText(R.string.todo_completed))
+        todoTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> showTodoSection(upcomingVisible = true)
+                    1 -> showTodoSection(upcomingVisible = false)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) = Unit
+            override fun onTabReselected(tab: TabLayout.Tab) = Unit
+        })
+        showTodoSection(upcomingVisible = true)
+    }
+
+    private fun showTodoSection(upcomingVisible: Boolean) {
+        upcomingContent.visibility = if (upcomingVisible) View.VISIBLE else View.GONE
+        completedContent.visibility = if (upcomingVisible) View.GONE else View.VISIBLE
     }
 
     private fun loadDashboard() {
